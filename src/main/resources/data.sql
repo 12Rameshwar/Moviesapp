381958d88f17fef9d935bb2a01855f869fca07d8
12Rameshwar <chavhanrameshwar13@gmail.com>
2024-10-21 15:49:30 +0530

    msg

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,13 @@
 INSERT INTO movies (title, genre, popular, poster_url) VALUES
-('The Shawshank Redemption', 'Drama', true, 'http://localhost:8080/image.png'),
-('The Godfather', 'Crime', true, 'http://localhost:8080/image1.png'),
-('Pulp Fiction', 'Crime', true, 'http://localhost:8080/image2.png');
+('The Shawshank Redemption', 'Drama', true, 'http://localhost:8080/api/image.png'),
+('The Godfather', 'Crime', true, 'http://localhost:8080/api/image1.png'),
+('Pulp Fiction', 'Crime', true, 'http://localhost:8080/api/image2.png'),
+('Iron Man', 'Crime',true,  'http://localhost:8080/api/image3.png'),
+('Avenger', 'Crime',true,  'http://localhost:8080/api/image4.png'),
+('Spiderman', 'Crime',true,  'http://localhost:8080/api/image5.png'),
+('Avtaar', 'Crime',true,  'http://localhost:8080/api/image6.png'),
+('Dr.Strange', 'Crime',true,  'http://localhost:8080/api/image7.png'),
+('Venom', 'Crime',true,  'http://localhost:8080/api/image8.png'),
+('Superman', 'Crime',true,  'http://localhost:8080/api/image9.png');
+
+
